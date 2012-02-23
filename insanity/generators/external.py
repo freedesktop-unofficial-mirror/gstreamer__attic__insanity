f52b6b72a712b84950e13499da58c3580a0bb7d0
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2012-02-23 13:36:43 +0000

    Add ExternalGenerator
    This can run any command line and reads its stdout.
    Go pipe crazy! Caveat you.

---
+++ insanity/generators/external.py
@@ -0,0 +1,91 @@
+# GStreamer QA system
+#
+#       generators/external.py
+#
+# Copyright (c) 2012, Collabora Ltd <vincent@collabora.co.uk>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+"""
+External process generator
+"""
+
+import os.path
+import subprocess
+
+from insanity.generator import Generator
+from insanity.log import debug, info, exception
+
+class ExternalGenerator(Generator):
+    """
+    Arguments:
+    * path to binary (could be a shell)
+    * arguments to binary (could be shell exec command)
+    * directory to run in
+
+    Returns:
+    * the process' stdout
+    """
+
+    __args__ = {
+        "command":"Command line to run",
+        "cwd":"Directory to run on",
+        }
+
+    # We don't know any semantics, derived classes are welcome to add some
+    __produces__ = None
+
+    def __init__(self, command="", cwd=None,
+                 *args,
+                 **kwargs):
+        """
+        command: Command line to run
+        cwd: Directory where to run the program
+        """
+        Generator.__init__(self, *args, **kwargs)
+        self.command = command
+        self.cwd = cwd
+        info("command:%r, cwd:%r" % (command, cwd))
+
+    def _generate(self):
+        debug("Running generator command line in %r: %r" % (self.cwd, self.command))
+        try:
+            process = subprocess.Popen([self.command],
+                                       stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                       cwd=self.cwd, shell=True, universal_newlines=True)
+        except Exception, e:
+            exception("Error running external generator: %r: %s", self.command, e)
+            return []
+
+        if process == None:
+            exception("Failed to create external generator process: %r", self.command)
+            return []
+
+        if process.stdin:
+            process.stdin.close()
+        if process.stderr:
+            process.stderr.close()
+
+        lines=[]
+        line=process.stdout.readline()
+        while line:
+            lines.append(line)
+            line=process.stdout.readline()
+        process.stdout.close()
+
+        info("Returning %d lines" % len(lines))
+        return lines
+
